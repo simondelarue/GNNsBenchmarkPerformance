a6a725c6b7a9939c6acd643decfbfe7b717ee596
simondelarue <sim.delarue@gmail.com>
2023-04-24 17:50:58 +0200

    added more baselines in get_model function

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ from baseline import Baseline
 
 def get_model(model: str) -> BaseModel:
     """Get model."""
-    if model.lower() in ['pagerank']:
+    if model.lower() in ['pagerank', 'labelpropagation', 'diffusion', 'knn']:
         return Baseline(model.lower())
     else:
         raise ValueError(f'Unknown model: {model}.')
