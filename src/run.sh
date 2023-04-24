2c976ce196fcbe5d3c55f1ee6e25e4c99f456123
simondelarue <sim.delarue@gmail.com>
2023-04-24 17:50:35 +0200

    for loop over k values in running script

--- src/run.sh
+++ src/run.sh
@@ -2,5 +2,9 @@
 
 for i in {3..15}
 do
+    # Baselines
     python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN
 done
