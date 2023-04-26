6965fb7f5715d322f621443d86e522a1d77a1bbe
simondelarue <sim.delarue@gmail.com>
2023-04-26 18:11:28 +0200

    added graphsage sampler

--- src/base.py
+++ src/base.py
@@ -7,6 +7,7 @@ class BaseModel(ABC):
 
     def __init__(self, name: str):
         self.name = name
+        self.train_loader = None
 
     @abstractmethod
     def fit_predict(self, dataset, train_idx: np.ndarray = None):
