96539e3d1cf500a1a1280bf0c64f9700e7a871af
simondelarue <sim.delarue@gmail.com>
2023-04-27 15:38:00 +0200

    updated runs for using features only

--- src/run.sh
+++ src/run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-for i in {10..10}
+for i in {3..10}
 do
     # Baselines
     # ---------
+    # Using Structure
+    # ---------------
     # Undirected + With penalization
     #python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank
     #python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation
     #python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion
     #python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN
-    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --embedding_method=true
+    #python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --embedding_method=true
 
     #python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank
     #python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation
@@ -25,6 +27,30 @@ do
     #python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation
     #python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion
     #python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN
+
+    # Using features instead of structure
+    # -----------------------------------
+    # Undirected + With penalization
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank --use_features=true
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation --use_features=true
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion --use_features=true
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --use_features=true
+    python main.py --dataset=cora --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --embedding_method=true --use_features=true
+
+    python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank --use_features=true
+    python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation --use_features=true
+    python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion --use_features=true
+    python main.py --dataset=citeseer --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --use_features=true
+
+    python main.py --dataset=pubmed --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank --use_features=true
+    python main.py --dataset=pubmed --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation --use_features=true
+    python main.py --dataset=pubmed --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion --use_features=true
+    python main.py --dataset=pubmed --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --use_features=true
+
+    python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=PageRank --use_features=true
+    python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=LabelPropagation --use_features=true
+    python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=Diffusion --use_features=true
+    python main.py --dataset=reddit --undirected=true --penalized=true --randomstate=8 --k=$i --stratified=true --model=KNN --use_features=true
 
     # Directed + with penalization
     # --> directed graph doest not exist in Sknetwork
