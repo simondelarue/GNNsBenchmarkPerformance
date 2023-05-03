e30c54ccacccc70698146ba0cd93625dfbe384e4
simondelarue <sim.delarue@gmail.com>
2023-05-03 09:01:45 +0200

    base class for dataset + Wikivitals dataset class

--- src/dataset.py
+++ src/dataset.py
@@ -3,7 +3,7 @@ import os
 import pickle
 from scipy import sparse
 
-from sklearn.model_selection import StratifiedKFold, KFold
+from base import BaseDataset
 
 from sknetwork.data import load_netset, Bunch
 
@@ -13,14 +13,87 @@ from torch_geometric.datasets import Planetoid, Reddit
 from torch_geometric.utils.convert import from_scipy_sparse_matrix
 
 
-class PlanetoidDataset:
+class WikivitalsDataset(BaseDataset):
+    """Wikivitals networks: Wikivitals, Wikivitals-fr, Wikischools."""
+
+    def __init__(self, dataset: str, undirected: bool, random_state: int, k: int, stratified: bool):
+        super(WikivitalsDataset, self).__init__(dataset, undirected, random_state, k, stratified)
+    
+    def get_netset(self, dataset: str, pathname: str, use_cache: bool = True):
+        """Get data in Netset format (scipy.sparse CSR matrices). Save data in Bunch format if use_cache is set to False.
+        
+        Parameters
+        ----------
+        dataset: str
+            Dataset name
+        pathname: str
+            Path to data.
+        use_cache: bool (default=True)
+            If True, use cached data (if existing).
+
+        Returns
+        -------
+            Bunch object.
+        """
+        if os.path.exists(os.path.join(pathname, dataset)) and use_cache:
+            with open(os.path.join(pathname, dataset), 'br') as f:
+                graph = pickle.load(f)
+            print(f'Loaded dataset from {os.path.join(pathname, dataset)}')
+        else:
+            print(f'Load netset data...')
+            graph_raw = load_netset(dataset)
+            
+            graph = Bunch()
+            graph.adjacency = graph_raw.adjacency
+            graph.biadjacency = graph_raw.biadjacency
+            graph.names = graph_raw.names
+            graph.labels_true = graph_raw.labels
+
+            # Save Netset dataset
+            with open(os.path.join(pathname, dataset), 'bw') as f:
+                pickle.dump(graph, f)
+            print(f'Netset data saved in {os.path.join(pathname, dataset)}')
+        
+        self.netset = graph
+        
+        return self.netset
+    
+    def get_data(self, dataset: str, undirected: bool):
+        """Get dataset information.
+        
+        Parameters
+        ----------
+        dataset: str
+            Dataset name
+        undirected: bool
+            If True dataset is forced to undirected.
+            
+        Returns
+        -------
+            Torch Dataset. """
+        if not undirected:
+            # Load netset
+            graph = load_netset(dataset)
+            adjacency = graph.adjacency
+            biadjacency = graph.biadjacency
+            names = graph.names
+            labels_true = graph.labels
+            # torch.Data object
+            data = Data(x=torch.FloatTensor(biadjacency.todense()),
+                        edge_index=from_scipy_sparse_matrix(adjacency)[0],
+                        y=torch.tensor(labels_true),
+                        num_classes=len(np.unique(labels_true)))
+        else:
+            raise Exception('Wikivitals dataset should be used with argument --undirected=true.')
+        
+        return data
+    
+    
+class PlanetoidDataset(BaseDataset):
     """Citation networks: Cora, PubMed, Citeseer."""
 
     def __init__(self, dataset: str, undirected: bool, random_state: int, k: int, stratified: bool):
-        self.random_state = random_state
-        self.data = self.get_data(dataset, undirected)
-        self.kfolds = self.k_fold(self.data, k, random_state, stratified)
-        self.netset = None
+        super(PlanetoidDataset, self).__init__(dataset, undirected, random_state, k, stratified)
 
     def get_netset(self, dataset: str, pathname: str, use_cache: bool = True):
         """Get data in Netset format (scipy.sparse CSR matrices). Save data in Bunch format if use_cache is set to False.
@@ -125,40 +198,3 @@ class PlanetoidDataset:
                test_mask=dataset.test_mask)
         
         return data
-
-    def k_fold(self, data: Data, k: int, random_state: int, stratified: bool = True) -> tuple:
-        """Split all data in Data into k folds. Each fold contains train/val/test splits, where val and test sizes equal 1/k.
-        
-        Parameters
-        ----------
-        data: Data
-            torch.Data wrapper containing graph and feature information.
-        k: int
-            k in k-folds method.
-        random_state: int
-            Controls the reproducility.
-        stratified: bool
-            If True, use stratified kfold.
-            
-        Returns
-        -------
-            Tuple of train/val/test indices for each fold.
-        """
-        if stratified:
-            skf = StratifiedKFold(k, shuffle=True, random_state=random_state)
-        else:
-            skf = KFold(k, shuffle=True, random_state=random_state)
-
-        test_indices, train_indices = [], []
-        for _, idx in skf.split(torch.zeros(len(data.x)), data.y):
-            test_indices.append(torch.from_numpy(idx).to(torch.long))
-
-        val_indices = [test_indices[i - 1] for i in range(k)]
-
-        for i in range(k):
-            train_mask = torch.ones(len(data.x), dtype=torch.bool)
-            train_mask[test_indices[i]] = 0
-            train_mask[val_indices[i]] = 0
-            train_indices.append(train_mask.nonzero(as_tuple=False).view(-1))
-        
-        return (train_indices, test_indices, val_indices)
